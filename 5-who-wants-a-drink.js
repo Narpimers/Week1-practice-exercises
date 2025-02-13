0d254f27f684e936fb33d2778001fa5bfa252e86
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-02-13 12:52:19 +0100

    slove an exercises 5-who-wants-a-drink with Yana

---
+++ 5-who-wants-a-drink.js
@@ -0,0 +1,32 @@
+/**
+ * You're at a party and you feel thirsty! However, you've got 5 friends who are also in need of a drink. Let's go get them a drink.
+ *
+ * Declare a variable that holds an empty array, called drinkTray.
+ * Create a loop that runs 5 times. On each iteration, push a drink into the drinkTray variable. The drinkTray can only hold at most two instances of the same drink type, for example it can only hold 2 colas, 2 lemonades, 2 waters.
+ * 
+ * Log to the console: "Hey guys, I brought a [INSERT VALUES FROM ARRAY]!" (For example: "Hey guys, I brought a cola, cola, lemonade, lemonade, water!")
+ */
+
+// There are 3 different types of drinks:
+
+const drinkTypes = ['cola', 'lemonade', 'water'];
+
+const drinkTray = [
+
+]
+//  let i = 0;
+
+// while (i < 5 ) {
+//     drinkTray.push(drinkTypes[]); i++
+// }
+//  console.log(`Hey guys, I brought a ${drinkTray}`);
+
+for (let key = 0; key < 3; key++) {
+    for (let i = 0; i < 2; i++) {
+        if (drinkTray.length<5){
+            drinkTray.push(drinkTypes[key]) ;
+        }
+    }
+}
+
+console.log(`Hey guys, I brought a ${drinkTray}`);
